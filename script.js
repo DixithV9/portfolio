8db6802e9d22ca757395df8aa46f067691d8cbb2
DixithV9 <deekshith@vrize.com>
2026-01-19 14:21:11 +0530

    css update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
     const navbar=document.querySelector(".navbar");
     window.addEventListener("scroll",()=>{
-        if (window.scrollY >50){
+        if (window.scrollY >10){
             navbar.classList.add("scrolled");
         }else{
             navbar.classList.remove("scrolled");
